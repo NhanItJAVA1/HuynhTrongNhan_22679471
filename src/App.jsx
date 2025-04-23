b63cbe09c0e1ee0f4527f5b8d99e6ecf3694946f
Huynh Trong Nhan <22679471.nhan@student.iuh.edu.vn>
2025-04-23 09:59:49 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Hello World</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          dem  {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
